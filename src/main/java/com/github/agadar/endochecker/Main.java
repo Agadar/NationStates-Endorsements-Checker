80f52d16791f29831f939012fe629a310a027ffc
Agadar <martin.donkersloot@outlook.com>
2017-01-27 00:31:56 +0100

    Implemented option to open nations to endorse in browser tabs.
    TODO: 
    - Only open nations that aren't already being endorsed;
    - Implement option to send telegrams to nations that aren't already endorsing specified nation.

--- src/main/java/com/github/agadar/endochecker/Main.java
+++ src/main/java/com/github/agadar/endochecker/Main.java
@@ -5,10 +5,16 @@ import com.github.agadar.nsapi.domain.nation.Nation;
 import com.github.agadar.nsapi.domain.region.Region;
 import com.github.agadar.nsapi.enums.shard.NationShard;
 import com.github.agadar.nsapi.enums.shard.RegionShard;
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Main entry for the CLI version of the endorsements checker.
@@ -18,10 +24,24 @@ import java.util.Scanner;
 public class Main {
 
     /**
+     * Prompts the user with the supplied question, which should expect a
+     * binary answer (yes or no), and returns whether the user answered yes.
+     * @param scanIn The scanner used for reading the user's answer.
+     * @param question The binary question to prompt the user.
+     * @return True if the user answered 'y' or 'yes', otherwise false.
+     */
+    private static boolean PromptYesOrNo(Scanner scanIn, String question) {
+        System.out.print(question + " (y/n?): ");
+        final String reply = scanIn.nextLine().toLowerCase();
+        return (reply != null && ("y".equals(reply) || "yes".equals(reply)));
+    }
+    
+    /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        NSAPI.setUserAgent("Agadar's Endorsements Checker (Testing)");
+        NSAPI.setUserAgent("Agadar's Endorsements Checker "
+                + "(https://github.com/Agadar/Endorsements-Checker)");
         final Scanner scanIn = new Scanner(System.in);
         boolean exit = false;
         
@@ -40,7 +60,7 @@ public class Main {
             if (region == null) {
                 System.err.println("Region does not exist!");
                 continue;
-            }           
+            }     
             final List<String> waNations = new ArrayList<>();
             List<String> endorsedBy = null;
             
@@ -88,15 +108,48 @@ public class Main {
             // Print info.
             System.out.println("# of WA members in region not already endorsing "
                     + "specified nation: " + waNations.size());
+            System.out.println();
             System.out.println("List:");
             
             // Sort list.
             Collections.sort(waNations);
             
             // Print resulting list.
-            waNations.stream().forEach((nationName) -> {
-                System.out.println("@" + nationName);
-            });
+            if (waNations.size() > 0) {
+                String listStr = waNations.get(0);
+                
+                for (int i = 1; i < waNations.size(); i++) {
+                    listStr += ", " + waNations.get(i);
+                }
+                System.out.println(listStr);
+                System.out.println();
+            }
+                    
+            // If requested, open browser tab for each nation.
+            if (PromptYesOrNo(scanIn, "Open browser tabs for each nation?")) {
+                // Make sure opening browser is supported by the system.
+                final Desktop desktop = Desktop.isDesktopSupported() ? 
+                        Desktop.getDesktop() : null;
+                
+                if (desktop != null && desktop.isSupported(Desktop.Action.BROWSE)) {
+                    try {
+                        // For each nation, open a browser tab.
+                        for (String nation : waNations) {
+                            desktop.browse(new URI("https://"
+                                    + "www.nationstates.net/nation=" + nation));
+                            // Required sleep, otherwise we get an error.
+                            Thread.sleep(1000);
+                        }
+                    } catch (URISyntaxException | IOException e) {
+                        e.printStackTrace();
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                } else {
+                    System.err.println("Opening browser tabs not supported on "
+                            + "your Operating System!");
+                }
+            }
         }
     }
     
